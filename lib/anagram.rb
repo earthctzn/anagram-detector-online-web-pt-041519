c58c61b0e6525b1dddc9c0fd23f842f79b775134
Caleb Aviles <earthctzn1@gmail.com>
2019-05-19 16:57:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,16 +5,16 @@ class Anagram
   def initialize(word)
     @word = word.split("")
   end
-  
-  # def match(array)
-  # ary = []
-  # array.each do |w| 
-  #     if w.split("").sort == @word.sort
-  #     ary << w
-  #     end  
-  #   end
-  #   ary
-  # end 
+  #longer version of .match
+  def match(array)
+  ary = []
+  array.each do |w| 
+      if w.split("").sort == @word.sort
+      ary << w
+      end  
+    end
+    ary
+  end 
 
   def match(array)
     array.select{|word|word.split("").sort == @word.sort}

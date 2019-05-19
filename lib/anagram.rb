8917c03ac6dcc088914226a20eadc528c0578179
Caleb Aviles <earthctzn1@gmail.com>
2019-05-19 16:55:27 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,17 +6,18 @@ class Anagram
     @word = word.split("")
   end
   
-  def match(array)
-   ary = []
-   array.each do |w| 
-      if w.split("").sort == @word.sort
-      ary << w
-      end  
-    end
-    ary
-  end 
+  # def match(array)
+  # ary = []
+  # array.each do |w| 
+  #     if w.split("").sort == @word.sort
+  #     ary << w
+  #     end  
+  #   end
+  #   ary
+  # end 
 
   def match(array)
-    array.select{|word|word.split("").sort == 
+    array.select{|word|word.split("").sort == @word.sort}
+  end
 
 end 
